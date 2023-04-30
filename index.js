5c172fd353c03cd96c19ae6ee0186a9b20d76e88
Manik Molla <mdblmanikislam777p@gmail.com>
2023-04-30 12:43:20 +0600

    vercel configfile added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000
 
 const catagories = require('./Data/catagories.json')
-
+const news = require('./Data/news.json')
 const cors = require('cors')
 
 app.use(cors())
@@ -16,6 +16,25 @@ app.get('/', (req, res) => {
 
 app.get('/catagories', (req, res) => {
     res.send(catagories)
+})
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectedNews = news.find( n => n._id === id);
+    res.send(selectedNews);
+})
+app.get('/catagories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    // console.log(id);
+    if( id == 0){
+        res.send(news);
+    }
+
+    const catagorieNews = news.filter( n => parseInt(n.category_id) === id);
+    res.send(catagorieNews);
 })
 
 app.listen(port, () => {
